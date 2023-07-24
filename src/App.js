0be9be6262c2fc58ce1646176954167798e0c7a4
Nicolás Sepúlveda Quiroz <nico@MacBook-Air.local>
2023-07-23 23:55:40 -0400

    deploy de v1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-
+import background from './img/movil 2.jpeg'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> I'm Nicolás S. Quiroz</h1>
+      <h2> Author: NSQ</h2>
+      {/* <img src={background} alt="background" /> */}
     </div>
   );
 }
