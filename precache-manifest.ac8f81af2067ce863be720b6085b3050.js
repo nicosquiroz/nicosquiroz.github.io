59d595ef95cc4fff8c561dc62e19e1042edc9e1f
Nicolás S. Quiroz <nicosquiroz@gmail.com>
2023-07-28 05:22:23 -0400

    Updates

---
+++ precache-manifest.ac8f81af2067ce863be720b6085b3050.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "78fc2ef1f6272666191b",
+    "url": "/static/css/main.417c196b.chunk.css"
+  },
+  {
+    "revision": "78fc2ef1f6272666191b",
+    "url": "/static/js/main.78fc2ef1.chunk.js"
+  },
+  {
+    "revision": "975f63afe5057efb9a20",
+    "url": "/static/js/1.975f63af.chunk.js"
+  },
+  {
+    "revision": "7bb5990819646a53a809",
+    "url": "/static/js/2.7bb59908.chunk.js"
+  },
+  {
+    "revision": "b9842c9c4c236a614e5d",
+    "url": "/static/js/runtime~main.b9842c9c.js"
+  },
+  {
+    "revision": "b8df56b0495ca70b27ce1656ec5c7d76",
+    "url": "/static/media/linea2.b8df56b0.png"
+  },
+  {
+    "revision": "1085b4085d5aeb5566b62a6c6f47e75a",
+    "url": "/static/media/gif.1085b408.gif"
+  },
+  {
+    "revision": "faa0cac829255874280ec5360e4bdfcf",
+    "url": "/index.html"
+  }
+];
