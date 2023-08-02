0f626481a011e6629fe762dbbbd75fd5503f2287
Nicolás S. Quiroz <nicosquiroz@gmail.com>
2023-08-02 01:33:03 -0400

    b

--- backend/main.py
+++ backend/main.py
@@ -53,7 +53,7 @@ origins = [
     "http://127.0.0.1:3000",
     "http://nicosquiroz.com",
     "https://nicosquiroz.com",
-    "nicosquiroz.com"
+    "nicosquiroz.com",
 ]
 
 # Agregar el middleware de CORS
