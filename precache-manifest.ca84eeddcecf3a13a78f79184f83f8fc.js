c391e042140ecfa22b0e4a039253301be9835124
Nicolás S. Quiroz <nicosquiroz@gmail.com>
2023-11-09 17:28:27 -0300

    Updates

--- precache-manifest.ca84eeddcecf3a13a78f79184f83f8fc.js
+++ precache-manifest.ca84eeddcecf3a13a78f79184f83f8fc.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "59d6b81fdfc68e2ba289",
-    "url": "/static/css/main.aa2368b9.chunk.css"
+    "revision": "00395b00709391678ce2",
+    "url": "/static/css/main.c434ad7c.chunk.css"
   },
   {
-    "revision": "59d6b81fdfc68e2ba289",
-    "url": "/static/js/main.59d6b81f.chunk.js"
+    "revision": "00395b00709391678ce2",
+    "url": "/static/js/main.00395b00.chunk.js"
   },
   {
     "revision": "fa677b773aaf13c72ca8",
@@ -64,16 +64,16 @@ self.__precacheManifest = [
     "url": "/static/media/movil 2.3f801a85.jpeg"
   },
   {
-    "revision": "8a36205bd9b83e03af0591a004bc97f4",
-    "url": "/static/media/Roboto-Regular.8a36205b.ttf"
+    "revision": "461d6a4a8fb43b2637e9f55b33022c41",
+    "url": "/static/media/Minimal-Mono-Regular.461d6a4a.otf"
   },
   {
     "revision": "2100b4a02ebb94c0aa18cabd642ee507",
     "url": "/static/media/RobotoSlab-Regular.2100b4a0.ttf"
   },
   {
-    "revision": "461d6a4a8fb43b2637e9f55b33022c41",
-    "url": "/static/media/Minimal-Mono-Regular.461d6a4a.otf"
+    "revision": "8a36205bd9b83e03af0591a004bc97f4",
+    "url": "/static/media/Roboto-Regular.8a36205b.ttf"
   },
   {
     "revision": "8caf9417629687b72b90d76ca161f2a5",
@@ -88,7 +88,7 @@ self.__precacheManifest = [
     "url": "/static/media/circulo.da67e089.png"
   },
   {
-    "revision": "43251d2958ddf18a610596d5d6a3565c",
+    "revision": "84a6b30bf0d59e463c799afd82aae6a9",
     "url": "/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.29e508d95634c3e1b2a66b50a182718d.js"
+  "/precache-manifest.ca84eeddcecf3a13a78f79184f83f8fc.js"
 );
 
 workbox.clientsClaim();
